636585813704612bc825ca32e72bfbb9226fa0f3
Haerun PC <haerunn@gmail.com>
2021-08-01 01:17:36 +0800

    feat: routes for menu page

--- application/config/routes.php
+++ application/config/routes.php
@@ -9,6 +9,7 @@ $route['translate_uri_dashes'] = FALSE;
 // member routes
 $route['user/member/daftar']['get'] = "user/daftar_member_page";
 $route['user/member/daftar']['post'] = "user/daftar_member";
+$route['user/menu']['get'] = "user/menu_page";
 
 /*
 | -------------------------------------------------------------------------
