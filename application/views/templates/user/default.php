9d1da62d685c71ebf70d62957e5ec077f2759731
Haerun PC <haerunn@gmail.com>
2021-07-31 23:53:27 +0800

    add: template for user member

---
+++ application/views/templates/user/default.php
@@ -0,0 +1,16 @@
+<?php
+	$this->load->view('templates/user/header.php', $data ?? null, FALSE);
+?>
+<!-- Begin Page Content -->
+<div class="container-fluid">
+	<!-- Page Heading -->
+	<h1 class="h3 mb-4 text-gray-800"><?= $title ?></h1>
+	<?php
+		$this->load->view("user/{$view_file}", $data ?? null, FALSE);
+	?>
+</div>
+<!-- /.container-fluid -->
+
+<?php
+	$this->load->view('templates/user/footer.php', $data ?? null, FALSE);
+?>
