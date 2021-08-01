4c1ce9dd042f2299378042009e3f9f70cd8097ac
Haerun PC <haerunn@gmail.com>
2021-08-01 10:49:48 +0800

    feat: add script to check cart item amount

--- application/views/templates/user/footer.php
+++ application/views/templates/user/footer.php
@@ -53,6 +53,10 @@
 
 	<!-- custom script -->
 	<script type="text/javascript" src="<?= base_url()."assets/js/custom.js" ?>"></script>
+
+	<script type="text/javascript">
+		get_cart_item_amount()
+	</script>
 </body>
 
 </html>
