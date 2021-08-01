98553be242c1c8580426239ef21a7cfaf6fca7d2
Haerun PC <haerunn@gmail.com>
2021-08-01 10:51:09 +0800

    feat: function to hitung total harga

--- application/models/MenuModel.php
+++ application/models/MenuModel.php
@@ -30,4 +30,24 @@ Class MenuModel extends CI_Model
 		$q = $this->db->get('menu');
 		return $result = $q->num_rows() > 0 ? $q->result_array() : array();
 	}
+
+	function pesanan_hitung_total_harga($post)
+	{
+		try {
+			// **
+			// first, we get the details of menu by accessing menu_get_list function
+			// this is where condition
+			$where = array();
+			$where['id_menu'] = $post['id_menu'];
+			$menu_list = $this->menu_get_list($where);
+			if (count($menu_list) < 1) {
+				throw new Exception('Menu tersebut tidak ada dalam database');
+			}
+			$menu_details = $menu_list[0];
+
+			return $total_harga = (int)$post['item_amount'] * (int)$menu_details['harga_menu'];
+		} catch (Exception $e) {
+			throw new Exception($e->getMessage());
+		}
+	}
 }
