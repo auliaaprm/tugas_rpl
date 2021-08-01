90cf17bf5894b0f2c5754cd94f0c36f7ebca345a
Haerun PC <haerunn@gmail.com>
2021-08-01 18:40:38 +0800

    feat: array to hash function

--- application/helpers/custom_helper.php
+++ application/helpers/custom_helper.php
@@ -4,4 +4,12 @@
 		print_r($data);
 		echo "</pre>";
 	}
+
+	function array_to_hash($key, $array) {
+		$hash = array();
+		foreach($array as $a) {
+			$hash[$a[$key]] = $a;
+		}
+		return $hash;
+	}
 ?>
