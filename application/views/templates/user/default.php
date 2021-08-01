e54ea6827067652ed2ae0f77a3557ef7b4a894eb
Haerun PC <haerunn@gmail.com>
2021-08-01 11:47:26 +0800

    refactor: title page using title_page variable or title

--- application/views/templates/user/default.php
+++ application/views/templates/user/default.php
@@ -4,7 +4,7 @@
 <!-- Begin Page Content -->
 <div class="container-fluid">
 	<!-- Page Heading -->
-	<h1 class="h3 mb-4 text-gray-800"><?= $title ?></h1>
+	<h1 class="h3 mb-4 text-gray-800"><?= $title_page ?? $title ?></h1>
 	<?php
 		$this->load->view("user/{$view_file}", $data ?? null, FALSE);
 	?>
