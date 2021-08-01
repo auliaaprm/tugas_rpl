428fe6e2c49ec993ab9c5a0fcf08940357f4ec11
Haerun PC <haerunn@gmail.com>
2021-08-01 21:37:12 +0800

    feat: show notif

--- application/views/templates/user/default.php
+++ application/views/templates/user/default.php
@@ -3,6 +3,11 @@
 ?>
 <!-- Begin Page Content -->
 <div class="container-fluid">
+	<?php if ($this->session->flashdata('notif')): ?>
+		<div class="alert alert-<?= $this->session->flashdata('notif')['result']; ?>" role="alert">
+			<?= $this->session->flashdata('notif')['message']; ?>
+		</div>
+	<?php endif ?>
 	<!-- Page Heading -->
 	<h1 class="h3 mb-4 text-gray-800"><?= $title_page ?? $title ?></h1>
 	<?php
