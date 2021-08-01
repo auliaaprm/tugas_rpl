5997d420ae58a250b858051b42710a8a7e186420
Haerun PC <haerunn@gmail.com>
2021-08-01 10:50:41 +0800

    style: add class unselectable

--- application/views/user/daftar_menu_page.php
+++ application/views/user/daftar_menu_page.php
@@ -1,12 +1,22 @@
+<h6>Lihat keranjang</h6>
 <div class="row">
 <?php foreach ($menu_list as $list): ?>
 	<div class="col-sm-6 col-lg-2 p-2">
-		<div class="card">
+		<div class="card" data-id="<?= $list['id_menu'] ?>">
 			<img class="card-img-top" src="<?= base_url()."assets/menu/${list['gambar_menu']}" ?>" alt="Card image cap">
 			<div class="card-body">
 				<h5 class="card-title"><strong><?= $list['nama_menu'] ?></strong></h5>
 				<p class="card-text">Rp <?= number_format($list['harga_menu'],0,".",".") ?></p>
-				<a href="#" class="btn btn-success btn-block">Tambah Pesanan</a>
+				<div class="input-group mb-3">
+					<div class="input-group-prepend">
+						<span class="input-group-text unselectable btn-decrease-item" style="cursor: pointer;">-</span>
+					</div>
+					<input type="number" class="form-control text-center amount-item" aria-label="Amount" value="1" min="1" max="100">
+					<div class="input-group-append">
+						<span class="input-group-text unselectable btn-add-item" style="cursor: pointer;">+</span>
+					</div>
+				</div>
+				<a href="#" class="btn-add-to-cart btn btn-success btn-block">Tambah ke keranjang</a>
 			</div>
 		</div>
 	</div>
