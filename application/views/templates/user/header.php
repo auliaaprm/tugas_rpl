ad841e4ccf02fb859299688f9efbae0abb71c209
Haerun PC <haerunn@gmail.com>
2021-08-01 09:39:49 +0800

    feat: custom.css

--- application/views/templates/user/header.php
+++ application/views/templates/user/header.php
@@ -15,9 +15,11 @@
 	<link href="<?= base_url('assets/') ?>vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
 	<link href="<?= base_url('assets/') ?>https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
-	<!-- Custom styles for this template-->
+	<!-- sb-admin-2 styles for this template-->
 	<link href="<?= base_url('assets/') ?>css/sb-admin-2.min.css" rel="stylesheet">
 
+	<!-- custom css -->
+	<link href="<?= base_url('assets/') ?>css/custom.css" rel="stylesheet">
 </head>
 
 <body id="page-top">
